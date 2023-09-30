7e078e9cf39233f4645c5f8289c40949e25b51e6
LUIS FERNANDO MONTOYA DIAZ <luismontoya164498@correo.itm.edu.co>
2023-09-30 14:01:48 -0500

    Branch1

---
+++ Veterinary.Shared/Entities/Owner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veterinary.Shared.Entities
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El documento es requerido")]
+        public String Documento { get; set; } = null;
+
+        [Required(ErrorMessage = "El primer nombre es requerido")]
+
+        public string FirstName { get; set; } = null;
+
+        [Required(ErrorMessage = "El apellido es requerido")]
+        public string LastName { get; set; } = null;
+
+        public string FixedPhone { get; set; } = null;
+
+        [Required(ErrorMessage = "El número del celular es requerido")]
+
+        public string CellPhone { get; set; } = null;
+
+        [Required(ErrorMessage = "La dirección es requerida")]
+        public string Address { get; set; } = null;
+
+
+        public string FullName => $"{FirstName}{LastName}";
+
+
+
+
+
+
+    }
+}
